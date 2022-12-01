0d0957e5f104fa006abde864103175f853ca8ff3
chunjae <rum33@chunjae.co.kr>
2022-12-01 11:05:46 +0900

    init structure

---
+++ src/main/java/com/chunjaeproject/team4/Team4Application.java
@@ -0,0 +1,13 @@
+package com.chunjaeproject.team4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Application.class, args);
+	}
+
+}
