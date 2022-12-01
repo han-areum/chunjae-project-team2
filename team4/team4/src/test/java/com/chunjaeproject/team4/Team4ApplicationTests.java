1e0d38cd93485109f03cdfdbb0e6d4dfe1af654c
chunjae <rum33@chunjae.co.kr>
2022-12-01 14:18:05 +0900

    init structure

---
+++ team4/team4/src/test/java/com/chunjaeproject/team4/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunjaeproject.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
